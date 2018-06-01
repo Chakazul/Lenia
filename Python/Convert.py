76d46cc9967b610b9b101825d6de39de8f0bdb0d
Bert Chan <36795411+Chakazul@users.noreply.github.com>
2018-06-01 17:50:03 +0800

    Add files via upload

---
+++ Python/Convert.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+import itertools
+
+def _unzip(st, is_shorten=True):
+	lines = st.split('/')
+	V = []
+	for l in lines:
+		nl = []
+		t = l.split('.')
+		if len(t) > 1:
+			nl.extend(_FromRepeatSt(t[0]) * [0])
+			cs = list(t[1])
+		else:
+			cs = list(t[0])
+		for c in cs:
+			v = _FromZip(c)
+			nl.append(int(v / 100 * 255))
+		if len(nl) > 0:
+			V.append(nl)
+	# print(V)
+	code = [ [' .' if v==0 else ' '+chr(ord('A')+v-1) if v<25 else chr(ord('p')+(v-25)//24) + chr(ord('A')+(v-25)%24) for v in row] for row in V]
+	if is_shorten:
+		rle = [ [(len(list(g)),k.strip()) for k,g in itertools.groupby(row)] for row in code]
+		for row in rle:
+			if row[-1][1]=='.': row.pop()
+		st = '$'.join(''.join([(str(n) if n>1 else '')+k for n,k in row]) for row in rle) + '!'
+	else:
+		st = '$'.join(''.join(row) for row in code) + '!'
+	return st
+
+def _FromZip(c):
+	return 0 if c=='0' else 100 if c=='1' else ord(c) - (192-1)
+def _IsZip(c):
+	return ord(c) >= 192
+def _FromRepeatSt(st):
+	return 1 if st=='' else _FromZip(st) if len(st)==1 else _FromZip(st[0])*100+_FromZip(st[1])
+
+with open('animals_old.json', encoding='utf-8') as file:
+	data = json.load(file)
+for d in data:
+	if 'name' in d: print(d['name'])
+	if 'cells' in d: d['cells'] = _unzip(d['cells'])
+with open('animals.json', 'w', encoding='utf-8') as file:
+	json.dump(data, file, separators=(',', ':'), ensure_ascii=False, indent=2)
